31c2ede03845161fa5f2732a42aab8513261a34c
SeYmUr867543210 <64531437+SeYmUr867543210@users.noreply.github.com>
2021-10-06 21:05:46 +0500

    removed api keys and token

--- server.js
+++ server.js
@@ -1,66 +1,88 @@
 const axios = require("axios");
 const TelegramBot = require("node-telegram-bot-api");
 
-// //youtube paramts
-const myApiKey = "AIzaSyAPx1ETtAaBo1qze17ryHo9TGb_DYCVaVs";
+//youtube paramts
+const myApiKey = "HERE IS YOUR YOUTUBE API KEY"; // change this to yours
 const baseApiUrl = "https://www.googleapis.com/youtube/v3";
-const channelName = "Samchik"; //
-const channelId = "UCHTWv0QpQ7HTDp_0gU4eJhQ";
-let allVideosId;
-const videosCount = 3;
-let prevVids = []; //all videos from a channel considering "resultsCount"
-let currentVids = [];
+const channelName = "HERE IS YOUR SERACHINGCHANNELS NAME"; // change this to yours
+let channelId;
+const responseResults = 50;
+
 
 //telegram paramts
-const TOKEN = "2026123924:AAGyg05JpAsdggz6vGnF2LwBCz5flSElsA0";
-let msgChatId;
+const TOKEN = "HERE IS YOUR TELEGRAM TOKEN"; // change this to yours
+let msgChatId = 00000000; //change it to yours(msg.chatId could be taken from bot.onmessage listener response)
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
 })
 
-bot.on("message", (msg) => {
-    msgChatId = msg.chat.id;
-})
 
-//using channelsId,gettings channel allVideosId
+//using channelName,getting channelId
+function getChannelId() {
+    let url = encodeURI(`${baseApiUrl}/search?key=${myApiKey}&type=channel&part=snippet&q=${channelName}`)
 
-function getVideosId() {
-    const url = `${baseApiUrl}/channels?key=${myApiKey}&id=${channelId}&part=contentDetails`
-    return axios.get(url)
-        .then(function(response) {
-            allVideosId = response.data.items[0].contentDetails.relatedPlaylists.uploads;
-            return allVideosId;
+    axios.get(url).then(function(response) {
+        response.data.items.forEach(element => {
+            if (element.snippet.title == channelName) {
+                channelId = element.id.channelId;
+                getAllVidsId(channelId);
+            }
         })
-};
-getVideosId().then(function(allVideosId) {
-    getVids(allVideosId);
-})
+    })
+}
+getChannelId()
 
+//using channelId,getting allVidsId
+let allVidsId;
 
+function getAllVidsId() {
+    let searchingFor = "contentDetails"
+    const url = `${baseApiUrl}/channels?key=${myApiKey}&id=${channelId}&part=${searchingFor}`;
 
+    axios.get(url).then(function(response) {
+        allVidsId = response.data.items[0].contentDetails.relatedPlaylists.uploads;
+        getAllVideosDetails(allVidsId);
+    })
+}
 
+let exArrObj = [];
+let nowArrObj = [];
 
-function getVids(allVideosId, repeatRequest) { //false
-    const url = `${baseApiUrl}/playlistItems?key=${myApiKey}&playlistId=${allVideosId}&maxResults=${videosCount}&part=snippet`
-    let videos = axios.get(url);
-    if (!repeatRequest) {
-        videos.then(response => {
-            pushDataToArrObj(response.data, prevVids);
-            getVids(allVideosId, "repeatRequest"); // true
+//using allVidsId, getting allVideosDetails(title,src)
+function getAllVideosDetails(allVidsId, repeatReqst) {
+    let searchingFor = "snippet";
+    let url = `${baseApiUrl}/playlistItems?key=${myApiKey}&playlistId=${allVidsId}&${responseResults}=30&part=${searchingFor}`;
+    let urlsAndTitles = axios.get(url);
+
+    if (!repeatReqst) {
+        urlsAndTitles.then(function(response) {
+            pushDataToArrObj(response.data, exArrObj);
+            getAllVideosDetails(allVidsId, true);
         })
-    } else {
-        videos.then(response => {
-            currentVids = [];
-            pushDataToArrObj(response.data, currentVids)
-            compareArrObjects();
-            getVids(allVideosId, "repeatRequest");
+    } else if (repeatReqst) {
+        urlsAndTitles.then(function(response) {
+            nowArrObj = [];
+            pushDataToArrObj(response.data, nowArrObj);
+            let changes = compareArrObjs(exArrObj, nowArrObj);
+
+            if (changes) {
+                tgBotSendALert(msgChatId, changes);
+                //exArrObj = arrObj;
+                // getAllVideosDetails(allVidsId, true);
+            } else {
+                console.log("repeatReq")
+                getAllVideosDetails(allVidsId, true);
+            }
+
         })
     }
+
+
 }
 
 function pushDataToArrObj(data, arrObj) {
-    data.items.forEach(function(item, i) {
+    data.items.forEach(function(item) {
         let vidTitle = item.snippet.title;
         let vidId = item.snippet.resourceId.videoId;
 
@@ -69,85 +91,49 @@ function pushDataToArrObj(data, arrObj) {
 
 }
 
-function compareArrObjects() {
+function compareArrObjs(exArrObj, arrObj) {
     function areEqual(obj1, obj2) {
         return obj1.title == obj2.title && obj1.src == obj2.src;
     }
 
-    let deleted = prevVids.filter(obj1 => currentVids.every(obj2 => !areEqual(obj1, obj2)))
-    let added = currentVids.filter(obj1 => prevVids.every(obj2 => !areEqual(obj1, obj2)))
+    let deleted = exArrObj.filter(obj1 => arrObj.every(obj2 => !areEqual(obj1, obj2)));
+    let added = arrObj.filter(obj1 => exArrObj.every(obj2 => !areEqual(obj1, obj2)));
 
-    if (deleted.length > 0) {
-        tgBotSendALert(deleted, "deleted")
-        prevVids = currentVids;
-        deleted = [];
-    }
-    if (added.length > 0) {
-        tgBotSendALert(added, "added")
-        prevVids = currentVids;
-        added = [];
-    }
-
+    return deleted.length || added.length ? { deleted, added } : false;
 }
 
-function tgBotSendALert(videos, youtubeVids) {
-    if (youtubeVids == "deleted") {
-        //send title to Telegram without src
-        for (let i = 0; i < videos.length; i++) {
+
+function tgBotSendALert(msgChatId, changes) {
+    if (changes.deleted.length) {
+        changes.deleted.map(function(item) {
             const html = `
-                    видео "<i>${videos[i].title}</i>" было удалено с youtube канала "<i>${searchingChannelsName}</i>"
-                `
+                                видео "<i>${item.title}</i>" было удалено с youtube канала "<i>${channelName}</i>"
+                            `
             bot.sendMessage(msgChatId, html, {
                 parse_mode: "HTML"
+            }).then(function() {
+                exArrObj = [];
+                for (let item of nowArrObj) {
+                    exArrObj.push(item)
+                }
+                getAllVideosDetails(allVidsId, true);
             })
-        }
-    } else if (youtubeVids == "added") {
-        //send title and src to Telegram
-        videos.forEach(element => {
+        })
+    } else if (changes.added.length) {
+        changes.added.map(function(item) {
             const html = `
-                        видео "<i>${element.title}</i>" было загружено в youtube канал "<i>${searchingChannelsName}</i>" ${element.src}
+                        видео "<i>${item.title}</i>" было загружено в youtube канал "<i>${channelName}</i>" ${item.src}
                     `
             bot.sendMessage(msgChatId, html, {
                 parse_mode: "HTML"
+            }).then(function() {
+                exArrObj = [];
+                for (let item of nowArrObj) {
+                    exArrObj.push(item)
+                }
+                getAllVideosDetails(allVidsId, true);
             })
-        });
+        })
     }
+
 }
-
-
-//ne mogu ponyat pochemu pri udalenii video s youtube,code zahodit v block "if (added.length > 0) na 82 strochke"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//2)sdelat tak chtob bez otpravki soobsheniya clientom mog uvedomlyat ob izmeneniyah na youtube kanale(posle pervoy otpravki klientom soobshenie,sohranyat msg.chat.id,a esli budut izmeneniya,to podstavlyat etot id i uvedomlyat)
-//3)kak sdelat tak chtob bot rabotal,daje esli moy komp vyklyuchen?
-
-// function repeatReqst() {
-//     let requestLimit = 10000; //that data could be taken from request
-//     let timeLimitInSecs = 24 * 60;//24 hours * 60 = seconds
-//     let quotaCostForRequest = 1;//that data could be taken from request
-//     let requestsAmount = 2;//kollichestvo zaprosov
-
-//     let id = setInterval(() => {
-//         getVids(allVideosId, "repeatRequest")
-//         // clearInterval(id);//
-
-//     }, (requestLimit / timeLimitInSecs / quotaCostForRequest / requestsAmount) * 1000);
-// }
